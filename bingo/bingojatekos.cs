6dfdb15e9fad96999e4b31960614d61bf38e5027
Ilián Molnár <molnar.ilian@petrik.hu>
2026-03-04 08:39:12 +0100

    class done

---
+++ bingo/bingojatekos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bingo
+{
+    internal class BingoJatekos
+    {
+        public string Nev;
+        public int[,] Kartya = new int[5, 5];
+        public bool[,] Talalatok = new bool[5, 5];
+
+        public BingoJatekos(string filenev)
+        {
+            this.Nev = Path.GetFileNameWithoutExtension(filenev);
+            string[] sorok = File.ReadAllLines(filenev);
+
+            for (int i = 0; i < 5; i++)
+            {
+                string[] elemek = sorok[i].Split(';');
+                for (int j = 0; j < 5; j++)
+                {
+                    if (elemek[j] == "X")
+                    {
+                        Talalatok[i, j] = true; 
+                    }
+                    else
+                    {
+                        Kartya[i, j] = int.Parse(elemek[j]);
+                        Talalatok[i, j] = false;
+                    }
+                }
+            }
+        }
+    }
+}
