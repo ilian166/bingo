2c494ab19aad13013120794735646ab9bd13232c
Ilián Molnár <molnar.ilian@petrik.hu>
2026-03-04 09:05:40 +0100

    3,4 done

--- bingo/Program.cs
+++ bingo/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-           
+            List<BingoJatekos> jatekosok = new List<BingoJatekos>();
+
+            string[] fajlnevek = File.ReadAllLines("nevek.text");
+
+            foreach (string fajl in fajlnevek)
+            {
+                if (jatekosok.Count < 100)
+                {
+                    jatekosok.Add(new BingoJatekos(fajl));
+                }
+            }
+
+            Console.WriteLine("4. feladat!");
+            Console.WriteLine($"A regisztrált játékosok száma: {jatekosok.Count}");
         }
     }
 }
